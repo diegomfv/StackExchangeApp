86175faf047106bd7cdcfde5ddd95202050add52
diegomfv <diego.fajardo.villena@gmail.com>
2020-08-15 22:30:54 +0100

    Add DetailActivity

---
+++ app/src/main/java/com/diegofajardo/stackexchangeapp/ui/detail/DetailActivity.kt
@@ -0,0 +1,14 @@
+package com.diegofajardo.stackexchangeapp.ui.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.diegofajardo.stackexchangeapp.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+
+}
