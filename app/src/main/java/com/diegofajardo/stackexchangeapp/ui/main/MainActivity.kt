8159ba7a0febfd95786c871f9f86ce2fb3c6c854
diegomfv <diego.fajardo.villena@gmail.com>
2020-08-15 11:55:53 +0100

    Prepare MainActivity layout (sketch)

--- app/src/main/java/com/diegofajardo/stackexchangeapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/diegofajardo/stackexchangeapp/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.diegofajardo.stackexchangeapp
+package com.diegofajardo.stackexchangeapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.diegofajardo.stackexchangeapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
