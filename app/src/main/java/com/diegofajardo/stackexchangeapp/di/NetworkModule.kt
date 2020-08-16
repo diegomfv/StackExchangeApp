4fccfabe370f7a378be5d903884f6197ecd60b38
diegomfv <diego.fajardo.villena@gmail.com>
2020-08-16 17:06:09 +0100

    Remove interceptor, remove okhttpclient from retrofit

--- app/src/main/java/com/diegofajardo/stackexchangeapp/di/NetworkModule.kt
+++ app/src/main/java/com/diegofajardo/stackexchangeapp/di/NetworkModule.kt
@@ -1,9 +1,7 @@
 package com.diegofajardo.stackexchangeapp.di
 
 import com.diegofajardo.stackexchangeapp.data.source.server.endpoint.StackExchangeServerEndpoints
-import okhttp3.ConnectionSpec
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -16,20 +14,11 @@ object NetworkModule {
 
     private val gsonConverterFactory by lazy { GsonConverterFactory.create() }
 
-    private val okHttpClient by lazy {
-        HttpLoggingInterceptor().run {
-            level = HttpLoggingInterceptor.Level.BODY
-            OkHttpClient.Builder()
-                .addInterceptor(this).build()
-        }
-    }
-
     val stackExchangeServerEndpoints : StackExchangeServerEndpoints by lazy {
         Retrofit.Builder()
             .baseUrl(baseUrl)
             .addCallAdapterFactory(rxJava2CallAdapterFactory)
             .addConverterFactory(gsonConverterFactory)
-            .client(okHttpClient)
             .build()
             .run {
                 create<StackExchangeServerEndpoints>(
