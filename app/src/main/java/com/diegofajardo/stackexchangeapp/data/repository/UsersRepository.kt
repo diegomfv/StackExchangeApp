9436dcf3e76825c3919c17ce10aac9e268972c98
diegomfv <diego.fajardo.villena@gmail.com>
2020-08-17 21:14:43 +0100

    Add documentation (UserRepository, GetUsersUsecase)

--- app/src/main/java/com/diegofajardo/stackexchangeapp/data/repository/UsersRepository.kt
+++ app/src/main/java/com/diegofajardo/stackexchangeapp/data/repository/UsersRepository.kt
@@ -5,8 +5,12 @@ import com.diegofajardo.stackexchangeapp.data.source.server.RemoteDataSource
 import com.diegofajardo.stackexchangeapp.domain.User
 import io.reactivex.Observable
 
+/**
+ * We use the repository to abstract the data source/s from the rest of the app.
+ * */
 class UserRepository(
     private val remoteDataSource: RemoteDataSource
+    //private val localDataSource: LocalDataSource
 ) {
 
     fun getUsers(queryModel: QueryModel): Observable<User> {
--- app/src/main/java/com/diegofajardo/stackexchangeapp/usecase/GetUsersUsecase.kt
+++ app/src/main/java/com/diegofajardo/stackexchangeapp/usecase/GetUsersUsecase.kt
@@ -5,6 +5,11 @@ import com.diegofajardo.stackexchangeapp.data.repository.UserRepository
 import com.diegofajardo.stackexchangeapp.domain.User
 import io.reactivex.Observable
 
+/**
+ * Even though the usecase looks redundant I left it here as an example.
+ * It allows to prepare the data for the viewmodel keeping the last one clean.
+ * Additionally, we gain scalability.
+ * */
 class GetUsersUsecase(
     private val userRepository: UserRepository
 ) {
